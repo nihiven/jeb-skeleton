a640f7b3eb5cf23f78b356bea2ea2c7dec3ede01
Justin Behanna <j.behanna@gmail.com>
2023-01-24 21:21:35 -0500

    working on resume and menu links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,9 @@
 <script>
-	import '../../src/nihiven.postcss';
+	import '../../src/nihiven.css';
 	import '@skeletonlabs/skeleton/styles/all.css';
 	import '../app.postcss';
 	import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+	import { menu } from '@skeletonlabs/skeleton';
 	import Footer from '../docs/footer.svelte';
 </script>
 
@@ -15,33 +16,64 @@
 				<strong class="text-xl"><a href="/">Justin Behanna</a></strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<!-- Divider -->
-				<span class="divider-vertical h-5" />
 				<LightSwitch />
-				<a
-					class="btn btn-sm btn-ghost-surface"
-					href="https://twitter.com/nihiven"
-					target="_blank"
-					rel="noreferrer">Twitter</a
-				>
-				<a
-					class="btn btn-sm btn-ghost-surface"
-					href="https://github.com/nihiven"
-					target="_blank"
-					rel="noreferrer">GitHub</a
-				>
-				<a
-					class="btn btn-sm btn-ghost-surface"
-					href="https://last.fm/user/b0x"
-					target="_blank"
-					rel="noreferrer">Last.fm</a
-				>
-				<a
-					class="btn btn-sm btn-ghost-surface"
-					href="https://www.linkedin.com/in/justin-behanna/"
-					target="_blank"
-					rel="noreferrer">LinkedIn</a
-				>
+				<span class="divider-vertical h-5" />
+				<section class="hidden lg:flex">
+					<!-- Resume -->
+					<a
+						class="unstyled hover:bg-primary-hover-token px-4 py-2 rounded-token"
+						href="/resume"
+						data-sveltekit-preload-data="hover">Resume</a
+					>
+					<!-- Projects -->
+					<div class="relative">
+						<button
+							class="unstyled hover:bg-primary-hover-token px-4 py-2 rounded-token space-x-2"
+							use:menu={{ menu: 'projects' }}
+						>
+							<span>Projects</span>
+							<i class="fa-solid fa-caret-down opacity-50" />
+						</button>
+						<div class="card overflow-hidden w-60 shadow-xl grid grid-cols-1" data-menu="projects">
+							<!-- Tailwind -->
+							<a
+								class="block space-y-4 p-4 hover:bg-primary-hover-token"
+								href="/elements/core"
+								data-sveltekit-preload-data="hover"
+							>
+								<h4>Tailwind</h4>
+								<small>Design tokens and CSS elements.</small>
+							</a>
+							<hr />
+							<!-- Svelte -->
+							<a
+								class="block space-y-4 p-4 hover:bg-primary-hover-token"
+								href="/actions/clipboard"
+								data-sveltekit-preload-data="hover"
+							>
+								<h4>Svelte</h4>
+								<small>Actions and Components.</small>
+							</a>
+							<hr />
+							<!-- Utilities -->
+							<a
+								class="block space-y-4 p-4 hover:bg-primary-hover-token"
+								href="/utilities/codeblocks"
+								data-sveltekit-preload-data="hover"
+							>
+								<h4>Utilities</h4>
+								<small>Powerful utility features.</small>
+							</a>
+						</div>
+					</div>
+					<!-- Blog -->
+					<a
+						class="unstyled hover:bg-primary-hover-token px-4 py-2 rounded-token"
+						href="/blog"
+						data-sveltekit-preload-data="hover">Blog</a
+					>
+				</section>
+				<!-- Divider -->
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,230 @@
+<h3 class="text-center">FORMATTING COMING SOON</h3>
+
+<!-- Header -->
+<header class="row-fluid">
+	<div class="title span7">
+		<!--<img src="assets/img/profile.jpg" title="jb" alt="My Profile"/>-->
+		<a href="http://justinbehanna.com"><h1>Justin Behanna</h1></a>
+		<!--<h2>programmer, team lead</h2>-->
+	</div>
+	<div class="social offset1 span4">
+		<ul>
+			<li><i class="fa fa-home" /> Pittsburgh, Pennsylvania</li>
+			<li><i class="fa fa-envelope" /> j.behanna@gmail.com</li>
+			<li><i class="fa fa-phone" /> 724.263.0728</li>
+			<li>
+				<a href="https://www.linkedin.com/in/justin-behanna/"><i class="fa fa-linkedin-square" /></a
+				>&nbsp;
+				<a href="https://www.twitter.com/nihiven/"><i class="fa fa-twitter-square" /></a>&nbsp;
+				<a href="https://github.com/nihiven/"><i class="fa fa-github-alt" /></a>&nbsp;
+				<a href="https://www.last.fm/user/b0x/"><i class="fa fa-music" /></a>
+				<a href="https://nvn.io"><i class="fa fa-home" /></a>
+			</li>
+		</ul>
+	</div>
+</header>
+
+<!-- Welcome Text
+			<article class="row-fluid">
+				<header class="span3">
+					<h3></h3>
+				</header>
+				<div class="span9 ">
+					<p class="welcome">“I have no idea where this will lead us, but I have a definite feeling it will be a place both wonderful and strange.”<br>
+					<p class="auth"> <i>Dale Cooper, Twin Peaks</i>  </p>
+				</div>
+			</article>
+			-->
+
+<!-- Work -->
+<article class="row-fluid">
+	<header class="span2">
+		<h3>Experience</h3>
+	</header>
+	<div class="span9">
+		<div class="span9">
+			<h5 class="pull-right">2022 - Present</h5>
+			<h4>
+				Senior Manager, IT Engineering<br />
+				Community Care Behavioral Health (UPMC)
+			</h4>
+			<h5>Application Development, Operations, Business Analysis, Tool Admin, QA.</h5>
+			<ul>
+				<li>Coming soon...</li>
+			</ul>
+
+			<br />
+
+			<h5 class="pull-right">2017 - 2022</h5>
+			<h4>Manager, System Support - Community Care Behavioral Health (UPMC)</h4>
+			<h5>Team management, EDI lead, and project planning.</h5>
+			<ul>
+				<li>Responsible for coordinating and delivering all EDI related projects</li>
+				<li>Perform employee reviews and interview candidates</li>
+				<li>
+					Responsible for transitioning aging hardware to virtual instances within UPMC data center
+				</li>
+				<li>Manage and report project status to leadership</li>
+				<li>IT Project Management for 500,000 member contract implementation</li>
+			</ul>
+
+			<br />
+			<h5 class="pull-right">2012 - 2016</h5>
+			<h4>Service Coordinator - Optuminsight, Inc.</h4>
+			<h5>
+				Accountable for managing the operational effectiveness of the Pittsburgh and Detroit claims
+				processing platforms.
+			</h5>
+			<ul>
+				<li>Subject matter expert for the Pittsburgh and Detroit Claims Processing Platforms</li>
+				<li>Supervised a group of 18 local, remote and offshore team members</li>
+				<li>Reported project status and various production metrics to executive management</li>
+				<li>Managed the prioritization and delegation of project assignments across the team</li>
+				<li>Responsible for resolving all batch failures and developing related break fix code</li>
+				<li>
+					Collaborated with project development team to ensure compliance with established
+					guidelines
+				</li>
+				<li>
+					Responsible for the transition from legacy platforms to UnitedHealth enterprise solutions
+				</li>
+				<li>
+					Accountable for decommission of the Pittsburgh and Detroit claims processing platforms
+				</li>
+			</ul>
+
+			<br />
+			<h5 class="pull-right">2009 - 2012</h5>
+			<h4>Application Development Team Lead - UnitedHealth Group</h4>
+			<h5>Managed the development of claims and eligibility related projects and break fixes.</h5>
+			<ul>
+				<li>Supervised a group of seven team members; five local and two remote</li>
+				<li>
+					Provided estimates for and managed implementation of project budgets of up to $3 million
+				</li>
+				<li>
+					Transitioned team to United SDLC including formal documentation, planning and reviews
+				</li>
+				<li>
+					Lead on integration into UnitedHealth IT claim platforms, data warehouse, and multiple
+					vendor integrations
+				</li>
+				<li>Headed transition from DTS to SSIS and SQL Server 2000 to 2005</li>
+				<li>Worked with HR team to interview and hire new team members</li>
+				<li>Responsible for IT compliance with CMS process audits</li>
+			</ul>
+
+			<br />
+			<h5 class="pull-right">2008 - 2009</h5>
+			<h4>Senior Application Developer - UnitedHealth Group</h4>
+			<h5>Implemented new lines of business and maintained existing processes.</h5>
+			<ul>
+				<li>Interviewed applicants and provided input during the hiring process</li>
+				<li>
+					Mentored junior team members and led code review sessions and business process reviews
+				</li>
+				<li>
+					Implemented the DC medical line of business, including 834 process and vendor interfaces
+				</li>
+				<li>Rewrote core EDI 834 processor, including PCP auto-assignment</li>
+				<li>IT liaison for Claims and Eligibility during annual CMS audits</li>
+			</ul>
+
+			<br />
+			<h5 class="pull-right">2006 - 2008</h5>
+			<h4>Application Developer - Unison Health Plan</h4>
+			<h5>Maintained eligibility batch processes and created new processes as required.</h5>
+			<ul>
+				<li>
+					Lead on the United expansion into Delaware, including coding 834 processor and batch load
+					process
+				</li>
+				<li>Programmer on the CHIP Adult basic expansion, updated existing code for 834 changes</li>
+				<li>Automated existing manual processes using Microsoft SQL Server DTS packages</li>
+			</ul>
+			<br />
+		</div>
+	</div>
+</article>
+
+<!-- Skills -->
+<article class="row-fluid">
+	<header class="span2">
+		<h3>Skills</h3>
+	</header>
+	<div class="span9">
+		<div class="row-fluid skills">
+			<ul class="span6">
+				<li><strong>script</strong> - python, perl, lua</li>
+				<li><strong>sql</strong> - tsql, psql, nosql</li>
+				<li><strong>web</strong> - js, react, angular, meteor</li>
+				<li><strong>code</strong> - c/c++, c#, .net, lua</li>
+				<li><strong>hacking</strong> - IDA, fiddler, x32/64dbg, metasploit</li>
+			</ul>
+			<ul class="span6">
+				<li><strong>os</strong> - windows, linux, OSX, VMWare</li>
+				<li><strong>sdlc</strong> - agile, scrum, iterative</li>
+				<li><strong>cloud</strong> - AWS, Azure</li>
+				<li><strong>source</strong> - Github, TFS, DevOps</li>
+				<li><strong>healthcare</strong> - 83x, 27x, Facets</li>
+			</ul>
+		</div>
+	</div>
+</article>
+
+<!-- Hobbies -->
+<article class="row-fluid">
+	<header class="span2">
+		<h3>Hobbies</h3>
+	</header>
+	<div class="span9">
+		<h4>Programming</h4>
+		<h5>
+			Strong desire to maintain a current skill set and develop projects outside of the work
+			environment.
+		</h5>
+		<ul>
+			<li>Developed games using python, lua, Unreal, and Unity. Currently developing action RPG</li>
+			<li>Long time IRC user. Written multiple chat clients and chat servers</li>
+			<li>Maintain personal website on AWS, built using hexo.io static site generator</li>
+			<li>Participates in game jams, such as <a href="https://ldjam.com/">Ludum Dare</a></li>
+			<li>Maintain a development blog at <a href="https://nvn.io">nvn.io</a></li>
+		</ul>
+
+		<br />
+		<h4>Misc.</h4>
+		<ul>
+			<!--<li>I'm a member of the South Hills Misfits Roller Hockey Club</li>-->
+			<li>
+				I play a lot of games, favorites; Fallout, Destiny, Diablo, GTA, Borderlands, Zelda: BOTW,
+				RDR2
+			</li>
+			<!--<li>Pittsburgh Penguins season ticket holder</li>-->
+			<li>I watch Miami Vice and Twin Peaks a lot</li>
+			<li>I saw space shuttle Endeavour take off for STS-134</li>
+			<li>One time I met John Carmack in Dallas</li>
+			<li>My favorite musical group is Depeche Mode</li>
+		</ul>
+	</div>
+</article>
+
+<!-- Education -->
+<article class="row-fluid">
+	<header class="span2">
+		<h3>Education</h3>
+	</header>
+	<div class="span9">
+		<h5 class="pull-right">2002 - 2006</h5>
+		<h4>California University of Pennsylvania</h4>
+		<h5>Applied Computer Science</h5>
+		<ul>
+			<li>Assistant Captain, Vulcans Ice Hockey</li>
+			<li>Computer Club Member, ACM Programming Contest Competitor</li>
+		</ul>
+	</div>
+</article>
+
+<!-- Footer -->
+<footer>
+	<a href="#" onclick="javascript:window.print();">Print</a>
+</footer>
