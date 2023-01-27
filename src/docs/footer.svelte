3069c5721b418cfe87e0c53b4f859641e2dd548e
Justin Behanna <j.behanna@gmail.com>
2023-01-27 16:51:59 -0500

    fix up the footer

--- src/docs/footer.svelte
+++ src/docs/footer.svelte
@@ -1,17 +1,18 @@
 <footer
 	class="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800"
 >
-	<span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
-		>© 2023 <a href="https://justinbehanna.com/" class="hover:underline">JustinBehanna.com</a> - All
-		Rights Reserved.
+	<span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+		<a href="https://www.youtube.com/watch?v=MFQS7kOCwoI" class="hover:underline">🐒</a> Built with
+		<a href="https://svelte.dev/" class="hover:underline">Svelte</a> and
+		<a href="https://tailwindcss.com/" class="hover:underline">Tailwind.</a>
 	</span>
 	<span>
-		<a href="https://github.com/nihiven/jeb-skeleton" class="hover:underline">Source</a>
+		<a href="https://github.com/nihiven/jeb-skeleton" class="hover:underline mx-1">Source</a>
 		<span class="divider-vertical h-2" />
-		<a href="https://twitter.com/nihiven" class="hover:underline">Twitter</a>
+		<a href="https://twitter.com/nihiven" class="hover:underline mx-1">Twitter</a>
 		<span class="divider-vertical h-2" />
-		<a href="https://www.linkedin.com/in/justin-behanna/" class="hover:underline">LinkedIn</a>
+		<a href="https://www.linkedin.com/in/justin-behanna/" class="hover:underline mx-1">LinkedIn</a>
 		<span class="divider-vertical h-2" />
-		<a href="https://last.fm/user/b0x" class="hover:underline">Last.fm</a>
+		<a href="https://last.fm/user/b0x" class="hover:underline mx-1">Last.fm</a>
 	</span>
 </footer>
