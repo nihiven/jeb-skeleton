c810c49a0533cf807539384243e7a2acdf1e17fb
Justin Behanna <j.behanna@gmail.com>
2023-01-26 23:21:21 -0500

    push for pc

--- src/routes/budgie/+page.server.ts
+++ src/routes/budgie/+page.server.ts
@@ -2,27 +2,46 @@ import { fail } from '@sveltejs/kit';
 import { API_KEY } from '$env/static/private';
 
 export const actions = {
-  default: async () => {
-    const response = await fetch(`https://data.mongodb-api.com/app/data-xjvrs/endpoint/data/v1/`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Request-Headers': '*',
-        'api-key': API_KEY,
-      },
-      body: JSON.stringify({
-        "collection": "config",
-        "database": "budgie",
-        "dataSource": "ClusterMain",
-        "projection": {
-            "_id": 1
-        }
-      })
-    });
-    const res = await response.json();
-    if (res.ok) return res;
-    return fail(403, (res.error);
-  }
-}
-https://kit.svelte.dev/docs/hooks#server-hooks
-https://learn.svelte.dev/tutorial/env-static-private
+	default: async () => {
+		console.log('actions.default');
+		const response = await fetch(`https://data.mongodb-api.com/app/data-xjvrs/endpoint/data/v1/`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				'Access-Control-Request-Headers': '*',
+				'api-key': API_KEY
+			},
+			body: JSON.stringify({
+				collection: 'config',
+				database: 'budgie',
+				dataSource: 'ClusterMain',
+				projection: {
+					_id: 1
+				}
+			})
+		});
+		const res = await response.json();
+		if (res.ok) {
+			console.log(res);
+			return res;
+		}
+		return fail(403, res.error);
+	}
+};
+// https://kit.svelte.dev/docs/hooks#server-hooks
+// https://learn.svelte.dev/tutorial/env-static-private
+/*
+curl test
+
+curl --location --request POST 'https://data.mongodb-api.com/app/data-xjvrs/endpoint/data/v1/action/findOne' \
+--header 'Content-Type: application/json' \
+--header 'Access-Control-Request-Headers: *' \
+--header 'api-key: ksE7997VswPfHfpO01HrgQ0unLpu5HCn2hiGr0M4tfhtlPlJhbbJQc5UdSb8n4jn' \
+--data-raw '{
+    "collection":"config",
+    "database":"budgie",
+    "dataSource":"ClusterMain",
+    "projection": {"key": 1, "value": 1, "_id": 0}
+}'
+
+*/
--- src/routes/budgie/+page.svelte
+++ src/routes/budgie/+page.svelte
@@ -1,2 +1,13 @@
 <script>
+	import { onMount } from 'svelte';
+	import { getBudgieData } from './budgie_service';
+
+	let budgieData;
+	onMount(async () => {
+		const res = await getBudgieData('version');
+		budgieData = res.json();
+		console.log(budgieData);
+	});
 </script>
+
+{budgieData}} tests
