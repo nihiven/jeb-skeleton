e17b4ad31cc9f4c0db5cd265bd380175dd8c5c0d
Justin Behanna <j.behanna@gmail.com>
2023-02-03 21:49:32 -0500

    lets merge

--- src/routes/api/blog/new/+page.server.ts
+++ src/routes/api/blog/new/+page.server.ts
@@ -2,6 +2,16 @@
 export const actions = {
 	compose: async ({request}) => {
     const data = await request.formData();
+
+		// TODO: check for errors
+		// if form error, highlight errors on form
+		// if server error, show error toast
+
+		// TODO: no errors
+		// create new post
+		// redirect to new post?
+
+
 		console.log(data.get('title'), data.get('content'));
 	}
 };
